78a8f9b1065674f523f71431f6ba7ecb6abcb0ff
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-09-27 01:50:28 -0300

    melhorias visuais

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var type = new Typed('.typing-text',{
     typeSpeed: 110,
     loop: true,
 });
+
+let themeColor = document.querySelectorAll('.theme-toggler span');
+themeColor.forEach(color => color.addEventListener('click', () => {
+    let background = color.style.background;
+    document.querySelector('body').style.background = background;
+}))
