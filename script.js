ff5bd41ccfb3f651dae7ba94f460f051435d85bb
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-09-27 01:54:29 -0300

    toggle adicionado

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ var type = new Typed('.typing-text',{
     loop: true,
 });
 
+let themeColor = document.querySelectorAll('.theme-toggler span');
+themeColor.forEach(color => color.addEventListener('click', () => {
+    let background = color.style.background;
+    document.querySelector('body').style.background = background;
+}))
